bb071e6857e3c787334a1646158b1e13deabf6d1
willy <willy@it.com>
2025-01-13 22:49:12 +0800

    watchEffect

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script setup>
-import { reactive, ref, watch } from 'vue'
+import { reactive, ref, watch, watchEffect } from 'vue'
 // const msg = ref("你好mmm")
 const msg = reactive({
   name: "你好mmm",
   age: 18
 })
+
+const counter = ref(0)
 watch(() => ({ ...msg }), (newValue, oldValue) => {
   console.log(newValue, oldValue)
 }, {
   immediate: true,
   deep: true
+})
+
+const watchStop = watchEffect(() => {
+  console.log(msg.name)
+  console.log(counter.value)
+  if (counter.value > 2) {
+    watchStop()
+  }
+
 })
 </script>
 
@@ -18,8 +29,10 @@ watch(() => ({ ...msg }), (newValue, oldValue) => {
   <router-view></router-view>
 
   <el-button @click="msg.name = '你好kkk'">默认</el-button>
+  <el-button @click="counter++">+1</el-button>
 
   <div>{{ msg.name }}</div>
+  <div>{{ counter }}</div>
 </template>
 
 <style scoped>
